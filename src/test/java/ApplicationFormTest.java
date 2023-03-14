030ec0aede5a9ad4d0b8d15be400d834fea56e87
Pavel Kozlov <cozlov.pavel@mail.ru>
2023-03-14 21:39:39 +0300

    Add test

---
+++ src/test/java/ApplicationFormTest.java
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ApplicationFormTest {
+
+        private WebDriver driver;
+
+        @BeforeAll
+        static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+    }
+
+        @BeforeEach
+        void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+    }
+
+        @AfterEach
+        void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+    @Test
+    void shouldPositiveTest(){
+        driver.get(" http://localhost:9999");
+        driver.findElements(By.tagName("input")).get(0).sendKeys("Сергеев Сергей");
+        driver.findElements(By.tagName("input")).get(1).sendKeys("+79209998877");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.tagName("p")).getText().trim();
+        assertEquals(expected, actual);
+        }
+}
